9db2f9f82dfd4074042f14e47372efe6c7b22dd5
Sanju Mondal <sanjumondal130105@gmail.com>
2024-06-01 17:12:38 +0530

    video 10, dark mode and light mode on accordion element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
       <Navbar title="ReactJS" />
       <div className="container">
-        <TextForm heading="Enter the text to Analyze below" />
+        {/* <TextForm heading="Enter the text to Analyze below" /> */}
+        <About/>
       </div>
     </>
   );
